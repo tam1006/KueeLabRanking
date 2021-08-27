038f17a0310d13c22ae416e0bb4bc25178fba8b2
tam1006 <tamusyun1006@gmail.com>
2021-08-27 19:13:01 +0900

    edit header

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |i|
-User.create(email: "#{i}@gmail.com", username: "#{i}", password: "123456", password_confirmation: "123456", lab: "example", year: rand(2018..2021))
-aa = rand(100)
-a = rand(100)
-b = rand(100)
-c = rand(100)
-d = rand(100)
-f = rand(100)
-gpa = (4.3*aa+4*a+3*b+2*c+1*d)/(aa+a+b+c+d+f)
-Grade.create(AA: aa, A: a, B: b, C: c, D: d, F: f, user_id: i, GPA: gpa.round(2))
+150.times do |i|
+  User.create(email: "#{i}@gmail.com", username: "#{i}", password: "123456", password_confirmation: "123456", lab: "example", year: rand(2018..2021))
+  aa = rand(100)
+  a = rand(100)
+  b = rand(100)
+  c = rand(100)
+  d = rand(100)
+  f = rand(100)
+  gpa = (4.3*aa+4*a+3*b+2*c+1*d)/(aa+a+b+c+d+f)
+  Grade.create(AA: aa, A: a, B: b, C: c, D: d, F: f, user_id: i, GPA: gpa.round(2))
 end
